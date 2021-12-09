d0e70844e09c39eb7413784bd3848a3c9a779da9
thunder down <p.axelfranz@gmail.com>
2021-12-09 14:19:25 +0100

    Début du tp 2018

--- L1/Python/TP-2018/code.py
+++ L1/Python/TP-2018/code.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import random
+cell = (2,3)
 
 def entre0etN(n):
     return random.randint(0,n)
@@ -17,5 +18,29 @@ def listeZeros(n):
     for i in range(n):
         lst += [0]
     return lst
+#print(listeZeros(3))
 
-print(listeZeros(3))        
+def plateauZeros(nblignes,nbcolonnes):
+    lst = []
+    for i in range(nblignes):
+        lst += [listeZeros(nbcolonnes)]
+    return lst
+#print(plateauZeros(3,2))
+
+def voisines_PI(cell):
+    lgn = cell[0]
+    col = cell[1]
+    return [(lgn+1,col+1),(lgn+1,col-1),(lgn-1,col+1),(lgn-1,col-1)]
+#print(voisines_PI(cell)
+
+def voisines_PI_alea(cell):
+    return elementAuHasard(voisines_PI(cell))
+#print(voisines_PI_alea(cell))
+
+def afficheParcoursNpas_PI(cell,nb_pas):
+    print(cell)
+    if nb_pas == 0:
+        return
+    else:
+        afficheParcoursNpas_PI(voisines_PI_alea(cell), nb_pas-1)
+print(afficheParcoursNpas_PI(cell,3))
